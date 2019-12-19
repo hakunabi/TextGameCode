d6dd2474f0d4975a2ac79e644379610134ed88cd
Student <Student@SMSHS-Tech10s-MacBook-Air.local>
2019-12-19 09:32:06 -0500

    Initial commit

---
+++ src/Questions.java
@@ -0,0 +1,31 @@
+public class Questions {
+    public static int Random1 = (int) (Math.random() * 5 + 0);
+    public static String[] questionBank1 = {"you're stupid", "you're retarded","hi","bye","doodoo"};
+
+    public static int Random2 = (int) (Math.random() * 5 + 0);
+    public static String[] questionBank2 = {"you're stupid", "you're retarded","hi","bye","doodoo"};
+
+    public static int Random3 = (int) (Math.random() * 5 + 0);
+    public static String[] questionBank3 = {"you're stupid", "you're retarded","hi","bye","doodoo"};
+
+    public static int Random4 = (int) (Math.random() * 5 + 0);
+    public static String[] questionBank4 = {"you're stupid", "you're retarded","hi","bye","doodoo"};
+
+    public static int Random5 = (int) (Math.random() * 5 + 0);
+    public static String[] questionBank5 = {"you're stupid", "you're retarded","hi","bye","doodoo"};
+
+    public static int Random6 = (int) (Math.random() * 5 + 0);
+    public static String[] questionBank6 = {"you're stupid", "you're retarded","hi","bye","doodoo"};
+
+    public static int Random7 = (int) (Math.random() * 5 + 0);
+    public static String[] questionBank7 = {"you're stupid", "you're retarded","hi","bye","doodoo"};
+
+    public static int Random8 = (int) (Math.random() * 5 + 0);
+    public static String[] questionBank8 = {"you're stupid", "you're retarded","hi","bye","doodoo"};
+
+    public static int Random9 = (int) (Math.random() * 5 + 0);
+    public static String[] questionBank9 = {"you're stupid", "you're retarded","hi","bye","doodoo"};
+
+    public static int Random10 = (int) (Math.random() * 5 + 0);
+    public static String[] questionBank10 = {"you're stupid", "you're retarded","hi","bye","doodoo"};
+}
---
+++ src/Randomizer.java
@@ -0,0 +1,14 @@
+public class Randomizer {
+
+//                public static
+//                public static
+//                public static int Random3 = (int) (Math.random() * 5 + 10);
+//                public static int Random4 = (int) (Math.random() * 5 + 15);
+//                public static int Random5 = (int) (Math.random() * 5 + 20);
+//                public static int Random6 = (int) (Math.random() * 5 + 25);
+//                public static int Random7 = (int) (Math.random() * 5 + 30);
+//                public static int Random8 = (int) (Math.random() * 5 + 35);
+//                public static int Random9 = (int) (Math.random() * 5 + 40);
+//                public static int Random10 = (int) (Math.random() * 5 + 45);
+
+}
